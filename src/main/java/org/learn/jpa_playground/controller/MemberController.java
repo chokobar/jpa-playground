7eecccb28e9386848a00d3b6cf026d862e98032b
chokobar89 <chokobar89@naver.com>
2025-05-01 11:54:55 +0900

    시스템 로그 레벨 설정

--- src/main/java/org/learn/jpa_playground/controller/MemberController.java
+++ src/main/java/org/learn/jpa_playground/controller/MemberController.java
@@ -1,16 +1,22 @@
 package org.learn.jpa_playground.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.learn.jpa_playground.service.MemberService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/member")
 public class MemberController {
+
+    //private final Logger log = LoggerFactory.getLogger(getClass()); @Slf4j 이걸로 대체
 
     @Autowired
     private MemberService memberService;

25bb08c1e1f53d29a431f955debeb489a2ac1ef5
chokobar89 <chokobar89@naver.com>
2025-05-05 17:38:14 +0900

    Spring Security 클래스 Spring Security에서 제공하는 해시 기반 암호화 클래스

--- src/main/java/org/learn/jpa_playground/service/MemberService.java
+++ src/main/java/org/learn/jpa_playground/service/MemberService.java
@@ -1,7 +1,34 @@
 package org.learn.jpa_playground.service;
 
+import lombok.RequiredArgsConstructor;
+import org.learn.jpa_playground.domain.MemberDomain;
+import org.learn.jpa_playground.dto.MemberDTO;
+import org.learn.jpa_playground.repository.MemberRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
+
 @Service
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final MemberRepository memberRepository;
+
+    public MemberDomain save(MemberDTO memberDto) {
+        MemberDomain memberDomain = MemberDomain.builder()
+                .id(memberDto.getId())
+                .password(passwordEncoder.encode(memberDto.getPassword()))
+                .name(memberDto.getName())
+                .age(memberDto.getAge())
+                .email(memberDto.getEmail())
+                .phone(memberDto.getPhone())
+                .createdDate(new Date())
+                .build();
+
+        return memberRepository.save(memberDomain);
+    }
+
 }

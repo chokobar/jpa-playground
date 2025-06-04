f0d550794d9c8700e8ac3487091f870645b5cc17
chokobar89 <chokobar89@naver.com>
2025-06-04 20:52:54 +0900

    JPA 회원 상세기능 findById는 Integer id가 기본인데 현재 프로젝트는 String인 userId로 하기때문에 수정

--- src/main/java/org/learn/jpa_playground/repository/MemberRepository.java
+++ src/main/java/org/learn/jpa_playground/repository/MemberRepository.java
@@ -4,6 +4,10 @@ import org.learn.jpa_playground.domain.MemberDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<MemberDomain, Integer> {
+
+    Optional<MemberDomain> findByUserId(String userId);
 }

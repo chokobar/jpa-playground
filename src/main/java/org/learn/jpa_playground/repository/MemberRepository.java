26d0cfc0ea3cdcb0b62d86ad85c17d77bb320956
chokobar89 <chokobar89@naver.com>
2025-06-11 20:25:46 +0900

    JPA delete 기능 작업중

--- src/main/java/org/learn/jpa_playground/repository/MemberRepository.java
+++ src/main/java/org/learn/jpa_playground/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package org.learn.jpa_playground.repository;
 
 import org.learn.jpa_playground.domain.MemberDomain;
+import org.learn.jpa_playground.dto.MemberDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,6 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<MemberDomain, Integer> {
 
     Optional<MemberDomain> findByUserId(String userId);
+
+    void deleteByUserId(String userId);
 }

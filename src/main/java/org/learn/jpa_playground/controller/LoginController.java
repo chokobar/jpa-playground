fc346a2d315de575aebae7edcc7b48da4d48e631
chokobar89 <chokobar89@naver.com>
2025-06-22 16:21:49 +0900

    로그인 기능

--- src/main/java/org/learn/jpa_playground/controller/LoginController.java
+++ src/main/java/org/learn/jpa_playground/controller/LoginController.java
@@ -1,17 +1,22 @@
 package org.learn.jpa_playground.controller;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.learn.jpa_playground.domain.MemberDomain;
 import org.learn.jpa_playground.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
+@RequestMapping("/auth")
 public class LoginController {
 
     @Autowired
@@ -22,14 +27,23 @@ public class LoginController {
         return "login";
     }
 
-    @PostMapping("login")
+    @PostMapping("/login-process")
     public String login(@RequestParam("userId") String userId,
-                        @RequestParam("userPassword") String userPassword, Model model) {
+                        @RequestParam("userPassword") String userPassword,
+                        HttpSession session,
+                        Model model) {
+        log.info("login start");
         log.info("userid : {}, userPassword : {}", userId, userPassword);
         try {
             MemberDomain member = loginService.login(userId, userPassword);
-            model.addAttribute("member", member);
-            return "Welcome to JPA Playground";
+            //model.addAttribute("member", member);
+            session.setAttribute("member", member);
+
+            UsernamePasswordAuthenticationToken auth =
+                    new UsernamePasswordAuthenticationToken(userId, null, null);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+
+            return "redirect:/";
         } catch (RuntimeException e) {
             model.addAttribute("error", e.getMessage());
             return "login";

88fbb018ef1e57248f97bcd30b601b3981be1d97
chokobar89 <chokobar89@naver.com>
2025-06-25 21:56:47 +0900

    메인페이지 수정

--- src/main/java/org/learn/jpa_playground/controller/HomeController.java
+++ src/main/java/org/learn/jpa_playground/controller/HomeController.java
@@ -1,15 +1,20 @@
 package org.learn.jpa_playground.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.learn.jpa_playground.domain.MemberDomain;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Slf4j
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(@SessionAttribute(value = "member", required = false) MemberDomain member,
+                       Model model) {
+        model.addAttribute("member", member);
         return "index";
     }
 

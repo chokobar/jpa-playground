6d217508c29aff9babbc28ee12532c5c146a0137
chokobar89 <chokobar89@naver.com>
2025-06-29 14:23:32 +0900

    게시판 JPA 실슬 개발 및 검증 메시지 개발 중

---
+++ src/main/java/org/learn/jpa_playground/domain/BoardDomain.java
@@ -0,0 +1,39 @@
+package org.learn.jpa_playground.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "board")
+public class BoardDomain {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "writer")
+    private String writer;
+
+    @Column(name = "created_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Column(name = "updated_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updated_date;
+
+    @Column(name = "view_count")
+    private Integer viewCount;
+}
---
+++ src/main/java/org/learn/jpa_playground/dto/BoardDTO.java
@@ -0,0 +1,30 @@
+package org.learn.jpa_playground.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BoardDTO {
+
+    private Long id;
+
+    @NotBlank(message = "{title.required}")
+    private String title;
+
+    @NotBlank(message = "{content.required}")
+    private String content;
+
+    private String writer;
+
+    private String createdDate;
+
+    private String updatedDate;
+
+    private Integer viewCount;
+}

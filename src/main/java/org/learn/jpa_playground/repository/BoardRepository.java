6b63c2268ffb860480c547413acd284847a7d630
chokobar89 <chokobar89@naver.com>
2025-07-14 21:10:51 +0900

    JPQL 사용시 주의 할점 주석 추가

--- src/main/java/org/learn/jpa_playground/repository/BoardRepository.java
+++ src/main/java/org/learn/jpa_playground/repository/BoardRepository.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BoardRepository extends JpaRepository <BoardDomain, Integer> {
 
+    // JPQL에서는 테이블명(board)과 컬럼명(ex:view_count 사용불가)이 아닌,
+    // 엔티티 클래스명(BoardDomain)과 필드명(ex:viewCount 사용가능)을 사용해야 한다.
     @Modifying
     @Query("UPDATE BoardDomain b SET b.viewCount = b.viewCount + 1 WHERE b.id = :id")
     int updateView(@Param("id") Long id);

44abdf03232116fe18084bacdb9994d9a8ab3909
chokobar89 <chokobar89@naver.com>
2025-07-29 21:41:14 +0900

    상품 테이블 설계 및 상품 로직 개발중

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -23,3 +23,17 @@ CREATE TABLE board (
    updated_date timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '수정일시',
    view_count INT DEFAULT 0 COMMENT '조회수'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- `jpa-playground`.`product` definition
+-- 상품 테이블
+CREATE TABLE product (
+     id BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '상품 ID (PK)',
+     name VARCHAR(255) NOT NULL COMMENT '상품명',
+     description TEXT COMMENT '상품 설명',
+     price INT NOT NULL COMMENT '상품 가격(원)',
+     stock_quantity INT DEFAULT 0 COMMENT '재고 수량',
+     status ENUM('AVAILABLE','SOLD_OUT','DISCONTINUED') DEFAULT 'AVAILABLE' COMMENT '상품 상태 (판매중, 품절, 단종)',
+     category VARCHAR(100) COMMENT '상품 카테고리 (예: 전자제품, 의류)',
+     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '등록일시',
+     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일시'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='상품 테이블';

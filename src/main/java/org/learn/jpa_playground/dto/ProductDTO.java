51263c968013505e5db107a88142d49dfe0bd3fe
chokobar89 <chokobar89@naver.com>
2025-08-04 20:03:27 +0900

    상품 등록시 Validation 작업

--- src/main/java/org/learn/jpa_playground/dto/ProductDTO.java
+++ src/main/java/org/learn/jpa_playground/dto/ProductDTO.java
@@ -1,6 +1,8 @@
 package org.learn.jpa_playground.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,12 +19,15 @@ public class ProductDTO {
     @NotBlank(message = "{name.required}")
     private String name;
 
+    @NotBlank(message = "{description.required}")
     private String description;
 
-    @NotBlank(message = "{price.required}")
+    @NotNull(message = "{price.required}")
+    @Min(value = 0, message = "가격은 0원 이상이어야 합니다.")
     private Integer price;
 
-    @NotBlank(message = "{stockQuantity.required}")
+    @NotNull(message = "{stockQuantity.required}")
+    @Min(value = 0, message = "재고 수량은 0 이상이어야 합니다.")
     private Integer stockQuantity;
 
     private ProductStatus status;  // ENUM 타입으로 선언

5483a772a4318c3a763d597369a1aa81b5d8f55b
chokobar89 <chokobar89@naver.com>
2025-08-12 07:00:47 +0900

    제품 상세페이지

--- src/main/java/org/learn/jpa_playground/repository/ProductRepository.java
+++ src/main/java/org/learn/jpa_playground/repository/ProductRepository.java
@@ -4,7 +4,10 @@ import org.learn.jpa_playground.domain.ProductDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<ProductDomain, Integer> {
 
+    Optional<ProductDomain> findById(Long id);
 }
